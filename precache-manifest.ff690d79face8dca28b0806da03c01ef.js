62c650c598dd548cfbeca6f0b728804608527c53
scodek <skhalequ03@gmail.com>
2020-05-18 10:27:29 +0200

    Updates

---
+++ precache-manifest.ff690d79face8dca28b0806da03c01ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/markdown-previewer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6ac31481741a1aafa92f",
+    "url": "/markdown-previewer/static/js/runtime~main.2b49566a.js"
+  },
+  {
+    "revision": "fe5b80e57be8d6ed4c98",
+    "url": "/markdown-previewer/static/js/main.549c224f.chunk.js"
+  },
+  {
+    "revision": "335f47ef67a08de7cc1c",
+    "url": "/markdown-previewer/static/js/2.443ead11.chunk.js"
+  },
+  {
+    "revision": "fe5b80e57be8d6ed4c98",
+    "url": "/markdown-previewer/static/css/main.bdb092c8.chunk.css"
+  },
+  {
+    "revision": "ff47e4adca7653d599568e3a8f54a857",
+    "url": "/markdown-previewer/index.html"
+  }
+];
